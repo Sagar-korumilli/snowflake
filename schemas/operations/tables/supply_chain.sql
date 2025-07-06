db579b462df83af69b0074ba0a201890aa2e1fed
Sagar-korumilli <93094340+Sagar-korumilli@users.noreply.github.com>
2025-07-07 04:57:57 +0530

    Update supply_chain.sql

--- schemas/operations/tables/supply_chain.sql
+++ schemas/operations/tables/supply_chain.sql
@@ -4,3 +4,9 @@ CREATE OR REPLACE TABLE operations.supply_chain (
     quantity INT,
     last_updated DATE
 );
+CREATE OR REPLACE TABLE operations.supply_chain2 (
+    item_id INT,
+    location STRING,
+    quantity INT,
+    last_updated DATE
+);
