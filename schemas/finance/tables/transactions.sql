40b6bfdf88b38af96a1311dd86cb2de5e85e928b
Sagar-korumilli <93094340+Sagar-korumilli@users.noreply.github.com>
2025-07-06 22:26:53 +0530

    Update transactions.sql

--- schemas/finance/tables/transactions.sql
+++ schemas/finance/tables/transactions.sql
@@ -4,3 +4,9 @@ CREATE OR REPLACE TABLE finance.transactions (
     amount NUMBER(12,2),
     transaction_date DATE
 );
+CREATE OR REPLACE TABLE finance.transactions123 (
+    transaction_id INT,
+    account_id INT,
+    amount NUMBER(12,2),
+    transaction_date DATE
+);
