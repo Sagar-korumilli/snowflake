dad45d7095e01587de9425230d7439cc2486040e
Sagar-korumilli <93094340+Sagar-korumilli@users.noreply.github.com>
2025-07-07 06:08:11 +0530

    Update employees.sql

--- schemas/hr/tables/employees.sql
+++ schemas/hr/tables/employees.sql
@@ -10,3 +10,9 @@ CREATE OR REPLACE TABLE hr.employees2 (
     department STRING,
     hire_date DATE
 );
+CREATE OR REPLACE TABLE hr.employees3 (
+    employee_id INT,
+    name STRING,
+    department STRING,
+    hire_date DATE
+);
